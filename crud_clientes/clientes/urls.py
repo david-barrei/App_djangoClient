f6ed3a6e73d9f70d90d27cb93817b92a1debbd42
david-barrei <davbarreiro4@gmail.com>
2023-09-11 01:03:34 -0500

    Primer commit

---
+++ crud_clientes/clientes/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+from django.conf import settings
+from django.contrib.staticfiles.urls import static
+
+urlpatterns = [
+    path('',views.inicio, name='inicio'),
+
+
+]+ static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT) #Para que muestre las imagenes
+
+
