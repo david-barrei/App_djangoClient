25e458c877297f31bcd63d70ab49d8a86a13e17d
david-barrei <davbarreiro4@gmail.com>
2023-09-23 22:53:14 -0500

    Crud completado

--- crud_clientes/clientes/urls.py
+++ crud_clientes/clientes/urls.py
@@ -6,7 +6,11 @@ from django.contrib.staticfiles.urls import static
 urlpatterns = [
     path('',views.inicio, name='inicio'),
     path('clientes/lista',views.lista, name='lista'),
-    path('clientes/crear', views.crear, name='crear')
+    path('<int:id>/', views.crear_editar, name='crear_editar'),
+    path('clientes/eliminar/<int:id>', views.eliminar, name='eliminar'),
+
+   
+
 
 
 ]+ static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT) #Para que muestre las imagenes
--- crud_clientes/clientes/views.py
+++ crud_clientes/clientes/views.py
@@ -12,15 +12,29 @@ def lista(request):
     clientes =Cliente.objects.all()
     return render(request,"Crud/listado.html",{"clientes":clientes})
 
-def crear(request):
-    if request.method=='GET':
-        formulario = ClienteForm()
-        return render (request,'Crud/crear.html', {'formulario':formulario})
+
+        
+def crear_editar(request, id=0):
+    if request.method== 'GET':
+        if id ==0:
+            formulario=ClienteForm()
+        else:
+            clienteid=Cliente.objects.get(pk=id)
+            formulario=ClienteForm(instance=clienteid)
+        return render (request, 'Crud/crear.html',{'formulario': formulario})
     else:
-        formulario=ClienteForm(request.POST)
+        if id==0:
+            formulario=ClienteForm(request.POST or None, request.FILES or None)
+        else:
+            clienteid = Cliente.objects.get(pk=id)
+            formulario = ClienteForm(request.POST or None, request.FILES or None, instance=clienteid)
         if formulario.is_valid():
             formulario.save()
-            return redirect('lista')
+        return redirect('lista')
 
+def eliminar(request, id):
+    bc=Cliente.objects.get(id=id)
+    bc.delete()
+    return redirect('lista')
         
         
